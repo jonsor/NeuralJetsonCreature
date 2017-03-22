34580d89968a140f6584f1565d145c59ab1e736f
Apostrophel <sjurbarndon@gmail.com>
2017-03-22 15:14:12 +0100

    HingeUpdate
    Added hinges to the cube class.

--- NeuralCreature/NeuralCreature/Cube.h
+++ NeuralCreature/NeuralCreature/Cube.h
@@ -16,23 +16,29 @@
 #include "Shader.h"
 #include "Camera.h"
 #include <btBulletDynamicsCommon.h>
+#include "Util.h"
+#include <vector>
+#include <map>
+#include "PhysicsManager.h"
 
 class Cube
 {
 private:
 	glm::vec3 position;
 	glm::vec3 color;
-	GLfloat with;
+	GLfloat width;
 	GLfloat height;
+	GLfloat depth;
 	GLuint cubeVAO;
 	GLfloat angle;
 	btScalar mass;
 	glm::vec3 axisOfRotation;
-
+	
 	//Bullet physics variables
-
 	btCollisionShape* collisionShape;
 	btRigidBody* rigidBody;
+	std::map<std::string, btHingeConstraint*> hinges;
+	const double PI = 3.141592653589793238463;
 
 
 public:
@@ -46,7 +52,8 @@ public:
 	void setUpPhysicsCube();
 	btRigidBody* getRigidBody();
 	void updatePhysics();
-	
+	void addHinge(glm::vec3 pivotA, glm::vec3 pivotB, glm::vec3 axisA, glm::vec3 axisB, Cube* cubeB, bool notCollision, PhysicsManager* pm, std::string name);
+	btHingeConstraint* getHinge(std::string name);
 	~Cube();
 };
 
